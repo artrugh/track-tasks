e5756703c51d935634f470b607ff80c9bcd79e2c
artrugh <arturo.rugh@gmail.com>
2021-12-09 10:23:05 +0100

    add-http-request

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,20 +1,17 @@
-// import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { TASKS } from '../mock-tasks';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 import { Task } from './../Task';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  // private apiUrl = 'http://localhost:5000/tasks';
-  // constructor(private http: HttpClient) {}
-  constructor() {}
+  private apiUrl = 'http://localhost:3000/tasks';
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    // return this.http.get<Task[]>(this.apiUrl);
-    const task = of(TASKS);
-    return task;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
